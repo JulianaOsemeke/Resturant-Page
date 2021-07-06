93c56e58bca8b59eda4584ff5a52888e4f27ad20
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-07-06 10:49:54 +0100

    Create home.js file and style home page

---
+++ src/homepage.js
@@ -0,0 +1,18 @@
+const home = () => {
+  const container = document.createElement("div");
+  container.className = 'homeNav'
+  
+
+  const tagline = document.createElement("h1");
+  tagline.innerText = "EXPLORE JAY'S DINER"
+  tagline.className = "tag"
+
+  
+
+  container.appendChild(tagline);
+  
+
+  return container
+}
+
+export default home
