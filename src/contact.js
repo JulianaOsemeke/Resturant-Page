d6553f9c5c83f35d26e7716c22e5ddeb3fa7222f
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-07-06 19:43:07 +0100

    Add contact form

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,34 @@
 const contact = () => {
   const container = document.createElement("div");
   container.className = 'contactNav'
+
   
   container.classList.add('hide')
   
   const paragraph = document.createElement("p");
-  paragraph.innerHTML = "Contact"
+  paragraph.innerHTML = "CONTACT US"
+  paragraph.className = "contactparagraph"
+
+  const form = document.createElement('form');
+  form.actionon = '#';
+  form.className = 'form-container'
+  form.innerHTML = 
+  ` 
+  <div>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter your email">
+</div>
+<div>
+<br>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter your order" ></textarea>
+</div>
+<br>
+<div>
+<input type="submit" value="Submit">
+</div>
+  `;
 
   container.appendChild(paragraph)
+  container.appendChild(form)
 
   return container
 }
