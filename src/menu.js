3f8a048b7f617899970420b22aefcf7bc1fd365f
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-07-08 11:26:11 +0100

    Correct Linter Errors

--- src/menu.js
+++ src/menu.js
@@ -1,38 +1,24 @@
-import { div } from "prelude-ls";
-
 const menu = () => {
-  
-  const container = document.createElement("div");
+  const container = document.createElement('div');
   container.className = 'menuNav'
-
-  container.classList.add('hide')
-
-  const paragraph = document.createElement("p");
-  paragraph.className = "menuTitle"
-  paragraph.innerHTML = "Menu"
-
-
+  container.classList.add('hide');
+  const paragraph = document.createElement('p');
+  paragraph.className = 'menuTitle';
+  paragraph.innerHTML = 'Menu';
   const divimage = new Image(200, 200);
-  divimage.setAttribute('src', './Assets/BangaSoup.jpeg')
-  
+  divimage.setAttribute('src', './Assets/BangaSoup.jpeg');
   const divimage2 = new Image(200, 200); 
-  divimage2.setAttribute('src', './Assets/Beanpudding.jpeg')
-  
-  const divimage3= new Image(200, 200); 
-  divimage3.setAttribute('src', './Assets/Ewariro.jpeg')
-
+  divimage2.setAttribute('src', './Assets/Beanpudding.jpeg');
+  const divimage3 = new Image(200, 200); 
+  divimage3.setAttribute('src', './Assets/Ewariro.jpeg');
   const divimage4 = new Image(200, 200); 
-  divimage4.setAttribute('src', './Assets/jollofrice.jpeg')
-
+  divimage4.setAttribute('src', './Assets/jollofrice.jpeg');
   const divimage5 = new Image(200, 200); 
-  divimage5.setAttribute('src', './Assets/riceandstew.jpeg')
-
+  divimage5.setAttribute('src', './Assets/riceandstew.jpeg');
   const divimage6 = new Image(200, 200); 
-  divimage6.setAttribute('src', './Assets/Yam.jpeg')
-
-
-  const menucon = document.createElement("div")
-  menucon.className = "menuList"
+  divimage6.setAttribute('src', './Assets/Yam.jpeg');
+  const menucon = document.createElement('div');
+  menucon.className = 'menuList';
   menucon.innerHTML = `
   <div class = "house">
   <div>
@@ -73,16 +59,15 @@ const menu = () => {
   <br>
   </house>
   `
-  container.appendChild(paragraph)
-  container.appendChild(divimage)
-  container.appendChild(divimage2)
-  container.appendChild(divimage3)
-  container.appendChild(divimage4)
-  container.appendChild(divimage5)
-  container.appendChild(divimage6)
-  container.appendChild(menucon)
-
-  return container
+  container.appendChild(paragraph);
+  container.appendChild(divimage);
+  container.appendChild(divimage2);
+  container.appendChild(divimage3);
+  container.appendChild(divimage4);
+  container.appendChild(divimage5);
+  container.appendChild(divimage6);
+  container.appendChild(menucon);
+  return container;
 }
 
-export default menu
+export default menu;
